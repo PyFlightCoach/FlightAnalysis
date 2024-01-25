0cdfda392aeb6ebc039978b3003db4daa385dd81
aespi234 <80226738+aespi234@users.noreply.github.com>
2024-01-25 08:28:03 -0600

    Add files via upload
    revision to make format consistent

--- examples/schedules_construction/AMA_Masters2024.py
+++ examples/schedules_construction/AMA_Masters2024.py
@@ -11,9 +11,11 @@ c45 = np.cos(np.radians(45))
 
 Mast24FC_def = SchedDef([
 
-    f3amb.create(ManInfo("Triang-1/4rolls", "trgle-1/4rolls", 3,Position.CENTRE,
-            BoxLocation(Height.TOP, Direction.UPWIND, Orientation.INVERTED),
-            BoxLocation(Height.MID)
+#####################################
+    f3amb.create(ManInfo(
+            "Triang-1/4rolls", "trgle-1/4rolls", 3,Position.CENTRE,
+            start=BoxLocation(Height.TOP, Direction.UPWIND, Orientation.INVERTED),
+            end=BoxLocation(Height.MID)
         ),[
             MBTags.CENTRE,
             f3amb.loop(r(1/8)),
@@ -26,17 +28,20 @@ Mast24FC_def = SchedDef([
             MBTags.CENTRE,
         ], line_length=150),
 
-    f3amb.create(ManInfo("half square-roll", "hSl-roll", 2, Position.END,
-            BoxLocation(Height.TOP, Direction.UPWIND, Orientation.INVERTED),
-            BoxLocation(Height.MID)
+#####################################
+    f3amb.create(ManInfo(
+            "half square-roll", "hSl-roll", 2, Position.END,
+            start=BoxLocation(Height.TOP, Direction.UPWIND, Orientation.INVERTED),
+            end=BoxLocation(Height.MID)
         ),[
             f3amb.loop(r(1/4)),
             centred(f3amb.roll(np.pi*2,line_length=2*55,padded=True)),
             f3amb.loop(r(1/4)),
         ]),
 
+#####################################
     f3amb.create(ManInfo(
-            "SLC-Rolls", "slc-rolls", k=4, position=Position.CENTRE, 
+            "SLC-Rolls", "slc-rolls", 5, position=Position.CENTRE, 
             start=BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.UPRIGHT),
             end=BoxLocation(Height.BTM)
         ),[
@@ -53,8 +58,9 @@ Mast24FC_def = SchedDef([
             MBTags.CENTRE, 
         ], line_length=80),
 
+#####################################
     f3amb.create(ManInfo(
-            "Figure 9-roll", "fig9-roll", k=3, position=Position.END, 
+            "Figure 9-roll", "fig9-roll", 3, position=Position.END, 
             start=BoxLocation(Height.TOP, Direction.DOWNWIND, Orientation.UPRIGHT),
             end=BoxLocation(Height.MID)
         ),[
@@ -63,16 +69,18 @@ Mast24FC_def = SchedDef([
             f3amb.loop(r(3/4)), 
         ], loop_radius = 40),
 
+#####################################
     f3amb.create(ManInfo(
-            "Roll Combo", "RollC", k=4, position=Position.CENTRE, 
+            "Roll Combo", "RollC", 4, position=Position.CENTRE, 
             start=BoxLocation(Height.MID, Direction.UPWIND, Orientation.UPRIGHT),
             end=BoxLocation(Height.TOP)
         ),[
             centred(f3amb.roll(r([0.25, 0.25, 0.25, -0.25, -0.25, -0.25]), padded=False)), 
         ]) ,
 
+#####################################
     f3amb.create(ManInfo(
-            "Stall Turn", "stall", k=3, position=Position.END, 
+            "Stall Turn", "stall", 3, position=Position.END, 
             start=BoxLocation(Height.BTM, Direction.UPWIND, Orientation.UPRIGHT),
             end=BoxLocation(Height.BTM)
         ),[
@@ -83,8 +91,9 @@ Mast24FC_def = SchedDef([
             f3amb.loop(-r(1/4)),
         ]),
 
+#####################################
     f3amb.create(ManInfo(
-            "Double Immelman", "dimmel", k=4, position=Position.CENTRE, 
+            "Double Immelman", "dimmel", 4, position=Position.CENTRE, 
             start=BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.INVERTED),
             end=BoxLocation(Height.BTM)
         ),[ 
@@ -98,9 +107,11 @@ Mast24FC_def = SchedDef([
             f3amb.line(length=180),
         ], loop_radius=90), 
 
-    f3amb.create(ManInfo("Humpty 1/2rolls", "humpty", 2, Position.END,
-            BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.UPRIGHT),
-            BoxLocation(Height.BTM)
+#####################################
+    f3amb.create(ManInfo(
+            "Humpty 1/2rolls", "humpty", 3, Position.END,
+            start=BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.UPRIGHT),
+            end=BoxLocation(Height.BTM)
         ),[
             f3amb.loop(np.pi*2/4),
             centred(f3amb.roll(r([0.5, -0.5]), padded=False)), 
@@ -109,8 +120,9 @@ Mast24FC_def = SchedDef([
             f3amb.loop(np.pi*2/4),
         ],loop_radius=30),
 
+#####################################
     f3amb.create(ManInfo(
-            "Loop-intgrolls ", "loopRlls", k=3, position=Position.CENTRE, 
+            "Loop-intgrolls ", "loopRlls", 5, position=Position.CENTRE, 
             start=BoxLocation(Height.MID, Direction.UPWIND, Orientation.UPRIGHT),
             end=BoxLocation(Height.MID)
         ),[
@@ -119,7 +131,7 @@ Mast24FC_def = SchedDef([
 
 #####################################
     f3amb.create(ManInfo(
-            "1/2SquareCorner-Rolls", "hscRolls", k=2, position=Position.END, 
+            "1/2SquareCorner-Rolls", "hscRolls", 2, position=Position.END, 
             start=BoxLocation(Height.TOP, Direction.UPWIND, Orientation.UPRIGHT),
             end=BoxLocation(Height.TOP)
         ),[
@@ -132,7 +144,7 @@ Mast24FC_def = SchedDef([
         ], loop_radius = 30,line_length=100),
 #####################################
     f3amb.create(ManInfo(
-            " 1/2 Cloverleaf-rolls", "hClovRolls", k=5, position=Position.CENTRE, 
+            " 1/2 Cloverleaf-rolls", "hClovRolls", 5, position=Position.CENTRE, 
             start=BoxLocation(Height.TOP, Direction.DOWNWIND, Orientation.INVERTED),
             end=BoxLocation(Height.TOP)
         ),[
@@ -149,7 +161,7 @@ Mast24FC_def = SchedDef([
 
 #####################################
     f3amb.create(ManInfo(
-            "Rev Fig Et-Rolls", "rEtRolls", k=3, position=Position.END, 
+            "Rev Fig Et-Rolls", "rEtRolls", 4, position=Position.END, 
             start=BoxLocation(Height.TOP, Direction.DOWNWIND, Orientation.UPRIGHT),
             end=BoxLocation(Height.TOP)
         ),[
@@ -161,7 +173,7 @@ Mast24FC_def = SchedDef([
         ], loop_radius = 45,line_length=85),
 #####################################
     f3amb.create(ManInfo(
-            "Spin2", "2Spin", k=3, position=Position.CENTRE, 
+            "Spin2", "2Spin", 3, position=Position.CENTRE, 
             start=BoxLocation(Height.TOP, Direction.UPWIND, Orientation.INVERTED),
             end=BoxLocation(Height.BTM),
         ),[      
@@ -171,9 +183,10 @@ Mast24FC_def = SchedDef([
             f3amb.loop(np.pi*2/4),            
         ], loop_radius = 30, line_length=95),
 #####################################
-    f3amb.create(ManInfo("Top Hat", "tophat", 3, Position.END,
-            BoxLocation(Height.TOP, Direction.UPWIND, Orientation.UPRIGHT),
-            BoxLocation(Height.TOP)
+    f3amb.create(ManInfo(
+            "Top Hat", "tophat", 3, Position.END,
+            start=BoxLocation(Height.TOP, Direction.UPWIND, Orientation.UPRIGHT),
+            end=BoxLocation(Height.TOP)
         ),[
             f3amb.loop(np.pi*2/4),
             centred(f3amb.roll(r([0.25, 0.25]), padded=True)),
@@ -184,16 +197,18 @@ Mast24FC_def = SchedDef([
             f3amb.loop(np.pi*2/4),
         ], loop_radius = 30,line_length=85),
 #####################################
-    f3amb.create(ManInfo("Fig Z -snap", "figzSnap", 4, Position.CENTRE,
-            BoxLocation(Height.TOP, Direction.DOWNWIND, Orientation.UPRIGHT),
-            BoxLocation(Height.TOP)
+    f3amb.create(ManInfo(
+            "Fig Z -snap", "figzSnap", 4, Position.CENTRE,
+            start=BoxLocation(Height.TOP, Direction.DOWNWIND, Orientation.UPRIGHT),
+            end=BoxLocation(Height.TOP)
         ),[
             f3amb.loop(np.pi*2*3/8),
             centred(f3amb.snap(r(1))),            
             f3amb.loop(-np.pi*2*3/8),
         ], loop_radius = 20,line_length=90),
 #####################################
-    f3amb.create(ManInfo("Comet-Rolls", "ComRolls", k=3, position=Position.END, 
+    f3amb.create(ManInfo(
+            "Comet-Rolls", "ComRolls", 3, position=Position.END, 
             start=BoxLocation(Height.TOP, Direction.DOWNWIND, Orientation.UPRIGHT),
             end=BoxLocation(Height.BTM)
         ),[
@@ -204,7 +219,8 @@ Mast24FC_def = SchedDef([
             f3amb.loop(np.pi*2/8), 
         ], loop_radius=45,line_length=100),
 #####################################
-    f3amb.create(ManInfo("Figure S-roll", "figSRoll", k=3, position=Position.CENTRE, 
+    f3amb.create(ManInfo(
+            "Figure S-roll", "figSRoll", 5, position=Position.CENTRE, 
             start=BoxLocation(Height.TOP, Direction.UPWIND, Orientation.UPRIGHT),
             end=BoxLocation(Height.BTM)
         ),[
@@ -230,5 +246,6 @@ if __name__ == "__main__":
 ##    Mast24FC_def.plot().show()
 
     Mast24FC_def.create_fcjs('AMA_Masters2024', f'{os.environ['HOME']}/Desktop/templates/')
+
 
 
