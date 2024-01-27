facca6823c0108e50ff84e867c2233e665a6fc7e
aespi234 <80226738+aespi234@users.noreply.github.com>
2024-01-27 06:14:27 -0600

    Add files via upload
    latest revision

--- examples/schedules_construction/AMA_Intermediate2024.py
+++ examples/schedules_construction/AMA_Intermediate2024.py
@@ -24,24 +24,24 @@ Intm24FC_def = SchedDef([
             f3amb.line(),
             f3amb.loop(r(1/8)),
             MBTags.CENTRE,
-        ], line_length=150),
+        ], loop_radius=40, line_length=150),
 
     f3amb.create(ManInfo("inv half square", "hSql", 2, Position.END,
             BoxLocation(Height.TOP, Direction.UPWIND, Orientation.INVERTED),
             BoxLocation(Height.MID)
         ),[
             f3amb.loop(r(1/4)),
-            f3amb.line(),
+            f3amb.line(length=105),
             f3amb.loop(r(1/4)),
-        ]),
+        ], loop_radius=50),
 
-    f3amb.create(ManInfo(
-            "2HorzRolls", "2hzrolls", k=4, position=Position.CENTRE, 
-            start=BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.UPRIGHT),
-            end=BoxLocation(Height.BTM)
+    f3amb.create(ManInfo("2HorzRolls", "hzrolls", 4, Position.CENTRE, 
+            BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.UPRIGHT),
+            BoxLocation(Height.BTM)
         ),[
             f3amb.roll(r(2), padded=False)
         ]),
+
     f3amb.create(ManInfo("half cuban", "hcuban", 2, Position.END,
             BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.UPRIGHT),
             BoxLocation(Height.BTM)
@@ -49,12 +49,11 @@ Intm24FC_def = SchedDef([
             f3amb.loop(r(5/8)),
             f3amb.roll(r(1/2)), 
             f3amb.loop(r(1/8)),
-        ], loop_radius = 60,line_length=120),
+        ], loop_radius = 65,line_length=155),
 
-   f3amb.create(ManInfo(
-            "Square on Corner", "sqLc", k=4, position=Position.CENTRE, 
-            start=BoxLocation(Height.BTM, Direction.UPWIND, Orientation.UPRIGHT),
-            end=BoxLocation(Height.BTM)
+   f3amb.create(ManInfo("Square on Corner", "sqLc", 4, Position.CENTRE, 
+            BoxLocation(Height.BTM, Direction.UPWIND, Orientation.UPRIGHT),
+            BoxLocation(Height.BTM)
         ),[
             MBTags.CENTRE,
             f3amb.loop(r(1/8)),
@@ -62,7 +61,7 @@ Intm24FC_def = SchedDef([
             f3amb.loop(r(1/4)),
             f3amb.line(),
             centred(f3amb.loop(r(1/4))),
-            f3amb.line(),
+           f3amb.line(),
             f3amb.loop(r(1/4)),
             f3amb.line(),
             f3amb.loop(r(1/8)),
@@ -74,25 +73,24 @@ Intm24FC_def = SchedDef([
             BoxLocation(Height.BTM)
         ),[
             f3amb.loop(np.pi*2/4),
-            f3amb.roll([np.pi*2]),
+            f3amb.roll([np.pi*2],line_length=150),
             f3amb.stallturn(),
-            f3amb.line(),
-            f3amb.loop(np.pi/2) 
-        ]),
+            f3amb.line(length=160),
+            f3amb.loop(np.pi/2), 
+        ], loop_radius = 45),
 
-    f3amb.create(ManInfo(
-            "Double Immelman", "dimmel", k=4, position=Position.CENTRE, 
-            start=BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.UPRIGHT),
-            end=BoxLocation(Height.BTM)
+    f3amb.create(ManInfo("Double Immelman", "dImm", 4, Position.CENTRE, 
+            BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.UPRIGHT),
+            BoxLocation(Height.BTM)
         ),[
             f3amb.loop(np.pi),
             f3amb.roll("1/2", padded=False),
-            f3amb.line(length=98),
+            f3amb.line(length=100),
             f3amb.loop(-np.pi),
             f3amb.roll("1/2", padded=False),
         ], loop_radius=100), 
 
-    f3amb.create(ManInfo("Humpty Bump", "humpty", 3, Position.END,
+    f3amb.create(ManInfo("Humpty Bump", "hB", 3, Position.END,
             BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.UPRIGHT),
             BoxLocation(Height.BTM)
         ),[
@@ -101,27 +99,26 @@ Intm24FC_def = SchedDef([
             f3amb.loop(np.pi*2/2),
             f3amb.line(),
             f3amb.loop(np.pi*2/4),
-        ]),
+        ],loop_radius = 45,line_length=120),
 
-    f3amb.create(ManInfo(
-            "2Loops", "loops", k=3, position=Position.CENTRE, 
-            start=BoxLocation(Height.BTM, Direction.UPWIND, Orientation.UPRIGHT),
-            end=BoxLocation(Height.BTM)
+    f3amb.create(ManInfo("2Loops", "loops", 3, Position.CENTRE, 
+            BoxLocation(Height.BTM, Direction.UPWIND, Orientation.UPRIGHT),
+            BoxLocation(Height.BTM)
         ),[
             f3amb.loop(4*np.pi),
-        ],loop_radius = 100),
+        ],loop_radius = 110),
 
-    f3amb.create(ManInfo(
-            "1/2Square on Corner", "hsc", k=4, position=Position.END, 
-            start=BoxLocation(Height.BTM, Direction.UPWIND, Orientation.UPRIGHT),
-            end=BoxLocation(Height.MID)
+    f3amb.create(ManInfo("Half Square on Corner", "hSqL2", 4, Position.END, 
+            BoxLocation(Height.BTM, Direction.UPWIND, Orientation.UPRIGHT),
+            BoxLocation(Height.MID)
         ),[
             f3amb.loop(np.pi/4),
             f3amb.line(),
             f3amb.loop(np.pi/2),
             f3amb.line(),
             f3amb.loop(np.pi/4),
-        ], line_length=50, loop_radius=40),
+        ], line_length=80, loop_radius=40),
+
 
     f3amb.create(ManInfo("Cuban Eight", "cuban", 2, Position.CENTRE,
             BoxLocation(Height.MID, Direction.DOWNWIND, Orientation.INVERTED),
@@ -142,42 +139,42 @@ Intm24FC_def = SchedDef([
             BoxLocation(Height.TOP)
         ),[
             f3amb.loop(np.pi*2*3/4),
-            f3amb.line(length=70),
+            f3amb.line(length=40),
             f3amb.loop(-np.pi*2/4),
-         ], loop_radius = 60),
+         ], loop_radius = 50),
 
     f3amb.create(ManInfo("Vert Dwl", "vertdl", 2, Position.CENTRE,
             BoxLocation(Height.TOP, Direction.UPWIND, Orientation.UPRIGHT),
             BoxLocation(Height.BTM)
         ),[
             f3amb.loop(-np.pi*0.5),
-            f3amb.line(length=110),
+            f3amb.line(length=125),
             f3amb.loop(np.pi*0.5),
         ]),
 
-    f3amb.create(ManInfo("Top Hat", "tophat", 2, Position.END,
+    f3amb.create(ManInfo("Top Hat", "tHat", 2, Position.END,
             BoxLocation(Height.BTM, Direction.UPWIND, Orientation.UPRIGHT),
             BoxLocation(Height.BTM)
         ),[
             f3amb.loop(np.pi*2/4),
             f3amb.roll([np.pi]), 
             f3amb.loop(np.pi*2/4),
-            f3amb.line(length=38),
+            f3amb.line(length=30),
             f3amb.loop(np.pi*2/4),
-            f3amb.line(),
+            f3amb.line(length=110),
             f3amb.loop(np.pi*2/4),
-        ]),
+        ], loop_radius = 45),
 
-    f3amb.create(ManInfo("Figure Z", "figz", 2, Position.CENTRE,
+    f3amb.create(ManInfo("Figure Z", "Z", 2, Position.CENTRE,
             BoxLocation(Height.BTM, Direction.DOWNWIND, Orientation.UPRIGHT),
             BoxLocation(Height.TOP)
         ),[
             f3amb.loop(np.pi*2*3/8),
             f3amb.line(c45), 
             f3amb.loop(-np.pi*2*3/8),
-        ], loop_radius = 25, line_length=120),
+        ], loop_radius =35, line_length=130),
 
-   f3amb.create(ManInfo("Split S", "splits", 2, Position.END,
+   f3amb.create(ManInfo("Split S", "splitS", 2, Position.END,
             BoxLocation(Height.TOP, Direction.DOWNWIND, Orientation.UPRIGHT),
             BoxLocation(Height.BTM)
         ),[
@@ -201,3 +198,4 @@ if __name__ == "__main__":
     #Intm24FC_def.plot().show()
 
     Intm24FC_def.create_fcjs('AMA_Intermediate2024', f'{os.environ['HOME']}/Desktop/templates/')
+
