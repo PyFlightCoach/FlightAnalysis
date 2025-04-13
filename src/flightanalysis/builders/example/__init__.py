a375d16a6b06869a43d009fe0594bbc13268b9e4
Thomas David <thomasdavid0@gmail.com>
2025-04-13 06:51:28 +0100

    example mb wip

--- src/flightanalysis/builders/example/__init__.py
+++ src/flightanalysis/builders/example/__init__.py
@@ -1,4 +1,4 @@
-from .criteria import F3A as Crit
+from .criteria import Crit
 from .box import box
 from .downgrades import dg_applicator
-from .manbuilder import f3amb as mb
+from .manbuilder import mb
