4a0ce0901de1752bad317c0752a3387dd18e6579
Thomas David <thomasdavid0@gmail.com>
2026-01-06 13:49:53 +0000

    reffunc descriptions

--- src/flightanalysis/scoring/reffuncs.py
+++ src/flightanalysis/scoring/reffuncs.py
@@ -7,20 +7,26 @@ smoothers = RFuncBuilders({})
 selectors = RFuncBuilders({})
 visor = RFuncBuilders({})
 
+rfuncs = dict(
+    measures=measures,
+    smoothers=smoothers,
+    selectors=selectors,
+    visor=visor,
+)
 
-@selectors.add
+@selectors.add("The last data point")
 def last(fl: State):
     """return the last index"""
     return [-1]
 
 
-@selectors.add
+@selectors.add("The first data point")
 def first(fl: State):
     """return the first index"""
     return [0]
 
 
-@selectors.add
+@selectors.add("A single specified index")
 def one(fl: State, i: int):
     """return the index i"""
     return [i]
