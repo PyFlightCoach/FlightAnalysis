f86ec3b93f1a2ee5fb92c13b02948e7dc34d2a6f
Thomas David <thomasdavid0@gmail.com>
2026-05-25 09:15:27 +0100

    change visibility weighting formula

--- src/flightanalysis/scoring/visibility.py
+++ src/flightanalysis/scoring/visibility.py
@@ -5,11 +5,15 @@ from typing import Literal
 def apply_visibility(val, factor: float, limit: float, kind: Literal["deviation", "value"] = 'value'):
     """factor between 0 and 1"""
 
-    b = 2.2 - factor * 1.2
+    with np.errstate(divide="ignore"):
+        b = 1 / factor
+
+    #res = (val / limit)**(1/factor)
+#    return np.nan_to_num(res)
 
     if kind == 'value':
         norm = np.abs(val / limit)
-        return np.where(norm > 1, norm, norm**b) * limit * np.sign(val)
+        return np.nan_to_num(np.where(norm > 1, norm, norm**b) * limit * np.sign(val))
     elif kind=='deviation':
         diff = np.insert(np.diff(val), 0, 0.0, axis=0)
         norm = np.abs(diff / limit)
@@ -17,8 +21,7 @@ def apply_visibility(val, factor: float, limit: float, kind: Literal["deviation"
         res = np.where(norm > 1, norm, norm**b) * limit * np.sign(diff) 
 
         smoothed = res.cumsum()
-        return smoothed - np.mean(smoothed) + np.mean(val)
-        return res.cumsum() + val[0]
+        return np.nan_to_num(smoothed - np.mean(smoothed) + np.mean(val))
     else:
         raise ValueError(f'kind {kind} not recognized')
 
