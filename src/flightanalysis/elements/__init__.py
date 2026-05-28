d3ebcfe35fba6c0e9b399a53cab0c962cbab55b8
Thomas David <thomasdavid0@gmail.com>
2026-05-28 07:35:59 +0100

    wip

--- src/flightanalysis/elements/__init__.py
+++ src/flightanalysis/elements/__init__.py
@@ -1,3 +1,4 @@
+from .tags import ElTag
 from .element import Element, Elements
 from .line import Line
 from .loop import Loop
@@ -5,5 +6,6 @@ from .stall_turn import StallTurn
 from .snap import Snap
 from .spin import Spin
 from .tail_slide import TailSlide
+
 
 type AnyElement = Line | Loop | StallTurn | Snap | Spin | TailSlide
